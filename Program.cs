801336657747ba6ba35f9068e2dce029e53d89d8
danyooo <danynorby0027@gmail.com>
2024-04-19 11:33:47 -0400

    Imported RestSharp Library

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+// use RestSharp library  to send requests to API's
+using RestSharp;
+
+
+namespace webAPI
+{
+  class Program
+  {
+    static void Main(string[] args)
+    // create a new Request 
+var RestRequest = new RestRequest();
+  }
+}
