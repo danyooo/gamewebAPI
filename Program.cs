97dfd1804f91401c5dec83a62d756752bb7cfb21
danyooo <danynorby0027@gmail.com>
2024-04-26 10:54:39 -0400

    Adds a method to print details of selected game (in the program class)

--- Program.cs
+++ Program.cs
@@ -7,6 +7,16 @@ namespace webAPI
 {
     class Program
     {
+         public static void printgameDetails(int randID, List<gameData> games)
+        {
+            Console.WriteLine($" gameID:{games[randID].id}");
+            Console.WriteLine($" Game is {games[randID].title}");
+            Console.WriteLine($"On {games[randID].platform}");
+            Console.WriteLine($"released on {games[randID].release_date}");
+            Console.WriteLine(games[randID].short_description);
+            Console.WriteLine($"Link: {games[randID].game_url}");
+
+        }
         static void Main(string[] args)
         {
             freeGames gameList = new freeGames();
@@ -21,11 +31,15 @@ namespace webAPI
             //set a random value to be seet to grab a random games data
             // TODO: Find an easier way to print all this data out? I.e [randomGameID].content
             int randomGameID = rnd.Next(0, allfreegames.Count-1);
-            Console.WriteLine("rndID is " + randomGameID);
-            Console.WriteLine($" Game ID is {allfreegames[randomGameID].id}");
-            Console.WriteLine($" Game ID is {allfreegames[randomGameID].title}");
-            Console.WriteLine($" Game ID is {allfreegames[randomGameID].short_description}");
-            Console.WriteLine($" Game ID is {allfreegames[randomGameID].game_url}");
+            printgameDetails(randomGameID,allfreegames) ;           
+            // Console.WriteLine("rndID is " + randomGameID);
+            // Console.WriteLine($" gameID:{allfreegames[randomGameID].id}");
+            // Console.WriteLine($" Game is {allfreegames[randomGameID].title}");
+            // Console.WriteLine($"On {allfreegames[randomGameID].platform}");
+
+            // Console.WriteLine(allfreegames[randomGameID].short_description);
+            // Console.WriteLine($"Link: {allfreegames[randomGameID].game_url}");
+            
 
         
         }
