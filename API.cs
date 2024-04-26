f721ff138522cad441f9eb2c0182108105dbb5d0
danyooo <danynorby0027@gmail.com>
2024-04-26 10:54:18 -0400

    Updates formatting

--- API.cs
+++ API.cs
@@ -1,27 +1,30 @@
 using System;
 using RestSharp;
-namespace webAPI{
-    // create a parent class to contain all features for API's in use.
+namespace webAPI
+{
+  // create a parent class to contain all features for API's in use.
 
-public abstract class API{
-  
-RestClient client;
-RestRequest APIrequest;
-public RestClient response;
-// set URL in child classes
- protected string url = "";
-//set params in child classes
- protected string parameters = "";
- // method to request information from an API
+  public abstract class API
+  {
 
-public RestResponse request(){
-  // get the client to visit the url provided
-  client = new RestClient(this.url);
-//Request the information that follows the parameters you set
-  APIrequest = new RestRequest(this.parameters);
-  // Get what the API responded with
-  var response = this.client.Get(this.APIrequest);
-  return response;
-}
-}
+    RestClient client;
+    RestRequest APIrequest;
+    public RestClient response;
+    // set URL in child classes
+    protected string url = "";
+    //set params in child classes
+    protected string parameters = "";
+    // method to request information from an API
+
+    public RestResponse request()
+    {
+      // get the client to visit the url provided
+      client = new RestClient(this.url);
+      //Request the information that follows the parameters you set
+      APIrequest = new RestRequest(this.parameters);
+      // Get what the API responded with
+      var response = this.client.Get(this.APIrequest);
+      return response;
+    }
+  }
 }
--- freetogameAPI.cs
+++ freetogameAPI.cs
@@ -1,11 +1,15 @@
 using System;
 using RestSharp;
-namespace webAPI{
-public class freeGames : API{
-    public freeGames(){
- this. url = "https://www.freetogame.com/api";
- this.parameters ="https://www.freetogame.com/api/games";
-    }
+namespace webAPI
+{
+    public class freeGames : API
+    {
+        public freeGames()
+        {
+            this.url = "https://www.freetogame.com/api";
+            this.parameters = "https://www.freetogame.com/api/games";
+        }
+       
 
-}
+    }
 }
